68744f86f5c32de742ba314a3f491c310f0bd36f
Wamula Bashir Saidi <wamulabash1@gmail.com>
2021-12-13 08:38:22 +0300

    worked on the graphs

--- backend/app/Http/Controllers/Api/Dashboard/DashboardController.php
+++ backend/app/Http/Controllers/Api/Dashboard/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\Dashboard;
 use App\Http\Controllers\BaseController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+Use \Carbon\Carbon;
 
 class DashboardController extends BaseController
 {
@@ -23,6 +24,9 @@ class DashboardController extends BaseController
             $projects = 0;
             $admins = 0;
             $schemes = 0;
+
+            $tomorrow= date("m-d-Y", strtotime("+1 day"));
+            $todayYearBack =date("m-d-Y", strtotime("-1 year"));
             if (Auth::user()->scheme_id) {
                 $contributors = DB::table('contributors')
                     ->where('scheme_id', Auth::user()->scheme_id)
@@ -36,6 +40,13 @@ class DashboardController extends BaseController
                 $projects = DB::table('projects')
                     ->where('scheme_id', Auth::user()->scheme_id)
                     ->count('id');
+                $contributorsGrowthGraph = DB::select("SELECT COUNT(C.id) AS total, EXTRACT(YEAR from C.created_at) AS year, EXTRACT(MONTH from C.created_at) AS month FROM contributors C
+                    WHERE C.created_at >= ? AND C.created_at <= ? AND C.scheme_id = ? GROUP BY EXTRACT(YEAR from C.created_at), EXTRACT(MONTH from C.created_at)
+                    ORDER BY EXTRACT(YEAR from C.created_at) DESC, EXTRACT(MONTH from C.created_at) DESC", [$todayYearBack, $tomorrow, Auth::user()->scheme_id]);
+
+                $totalContributions = DB::select("SELECT SUM(C.amount) AS total, EXTRACT(YEAR from C.created_at) AS year, EXTRACT(MONTH from C.created_at) AS month FROM contributions C
+                    WHERE C.created_at >= ? AND C.created_at <= ? AND status = ? AND C.scheme_id = ? GROUP BY EXTRACT(YEAR from C.created_at), EXTRACT(MONTH from C.created_at)
+                    ORDER BY EXTRACT(YEAR from C.created_at) DESC, EXTRACT(MONTH from C.created_at) DESC", [$todayYearBack, $tomorrow, true, Auth::user()->scheme_id]);
             } else {
                 $contributors = DB::table('contributors')
                     ->count('id');
@@ -49,6 +60,13 @@ class DashboardController extends BaseController
                     ->count('id');
                 $schemes = DB::table('schemes')
                     ->count('id');
+                $contributorsGrowthGraph = DB::select("SELECT COUNT(C.id) AS total, EXTRACT(YEAR from C.created_at) AS year, EXTRACT(MONTH from C.created_at) AS month FROM contributors C
+                    WHERE C.created_at >= ? AND C.created_at <= ? GROUP BY EXTRACT(YEAR from C.created_at), EXTRACT(MONTH from C.created_at)
+                    ORDER BY EXTRACT(YEAR from C.created_at) DESC, EXTRACT(MONTH from C.created_at) DESC", [$todayYearBack, $tomorrow]);
+
+                $totalContributions = DB::select("SELECT SUM(C.amount) AS total, EXTRACT(YEAR from C.created_at) AS year, EXTRACT(MONTH from C.created_at) AS month FROM contributions C
+                    WHERE C.created_at >= ? AND C.created_at <= ? AND status = ? GROUP BY EXTRACT(YEAR from C.created_at), EXTRACT(MONTH from C.created_at)
+                    ORDER BY EXTRACT(YEAR from C.created_at) DESC, EXTRACT(MONTH from C.created_at) DESC", [$todayYearBack, $tomorrow, true]);
             }
             return $this->sendResponse(
                 true,
@@ -60,7 +78,9 @@ class DashboardController extends BaseController
                     'branches' => $branches,
                     'admins' => $admins,
                     'contributors' => $contributors,
-                    'schemes' => $schemes
+                    'schemes' => $schemes,
+                    'contributorsGrowthGraph' => $contributorsGrowthGraph,
+                    'totalContributions'=> $totalContributions,
                 ]
             );
         } catch (\Exception $ex) {
